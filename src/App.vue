03d8042731f222d63b66172f1486e38f3877b892
matteo-cota <matteocota@outlook.it>
2024-09-16 18:17:10 +0200

    Add HeaderComponent - install Sass

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
     <HeaderComponent />
-    <BlueBarComponent />
-    <MainContent />
-    <FooterComponent />
+    
+    
+    
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
-import BlueBarComponent from './components/BlueBarComponent.vue';
-import MainContent from './components/MainContent.vue';
-import FooterComponent from './components/FooterComponent.vue';
+
+
 
 export default {
   components: {
     HeaderComponent,
-    BlueBarComponent,
-    MainContent,
-    FooterComponent,
+    
+    
   },
 };
 </script>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,28 +1,46 @@
 <template>
-    <header>
-      <div class="logo">
-        <img src="/img/dc-logo.png" alt="Logo DC" />
-      </div>
-      <nav>
-        <ul>
-          <li v-for="(item, index) in menuItems" :key="index">
-            <a href="#">{{ item }}</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        menuItems: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'],
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  
-  </style>
+  <header>
+    <div class="logo">
+      <img src="/img/dc-logo.png" alt="DC Logo" />
+    </div>
+    <nav>
+      <ul>
+        <li v-for="(item, index) in menuItems" :key="index">
+          <a href="#">{{ item }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  background-color: white;
+}
+.logo img {
+  height: 60px;
+}
+nav ul {
+  display: flex;
+  list-style: none;
+  gap: 20px;
+}
+nav ul li a {
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+}
+</style>
