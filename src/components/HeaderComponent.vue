9be169bb8d2f197755fbd02ef6add4a46664f272
matteo-cota <matteocota@outlook.it>
2024-09-16 17:58:09 +0200

    Adedd Components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+      <div class="logo">
+        <img src="/img/dc-logo.png" alt="Logo DC" />
+      </div>
+      <nav>
+        <ul>
+          <li v-for="(item, index) in menuItems" :key="index">
+            <a href="#">{{ item }}</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        menuItems: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
