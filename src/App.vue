5f543702869f7925d2fcd3050f792568dd7493f0
matteo-cota <matteocota@outlook.it>
2024-09-16 18:28:20 +0200

    Added Main Content

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
     <HeaderComponent />
-    
-    
+    <MainContent />    
     
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
-
-
+import MainContent from './components/MainContent.vue';
 
 export default {
   components: {
     HeaderComponent,
-    
-    
+    MainContent,    
   },
 };
 </script>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="main-content">
+      <h2>-->Content goes here<--</h2>
+    </section>
+  </template>
+  
+  <style scoped>
+  .main-content {
+    background-color: #1c1c1c;
+    color: white;
+    text-align: center;
+    padding: 50px 0;
+  }
+
+  h2 {
+    text-align: left;
+  }
+  </style>
